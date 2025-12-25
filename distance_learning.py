37b5291fd046fc3ad14d743c3627017c331a019f
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2025-12-25 14:13:55 +0100

    Scripts to run directly experiments

--- distance_learning.py
+++ distance_learning.py
@@ -1,5 +1,6 @@
 
 import torch
+import argparse
 from src.model import *
 from scripts.evaluate import Evaluator
 from src.dataset import *
@@ -11,89 +12,81 @@ from scripts.train import myTrainerDistance
  
 
 if __name__ == "__main__":
+    """
+    Load Parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-exp_name", "--experiment_name", help="Experiment name for logging", type=str, required=True)
+    args = parser.parse_args()
+    print(args)
+
     torch.cuda.empty_cache()
+    """
+    Load Config file and Logger
+    """
     config = myConfig()
     logger = myLogger(logger_name=__name__, config=config).get_logger()
     torch.manual_seed(config.rand_seed)
 
-    # Load Data
-    # variable = "K"
-    # Support Points
-    sp_variable_values = [20]
-    # K values KNN
-    # k_variable_values = [5, 7, 11]
-    k_val = 11
-    neg_margin_values = [1.0, 1.1, 1.2, 1.3]
-    
-    merged_variable_values = []
-
     metrics_lists  = {"train_acc": [],
                       "val_acc": [],
                       "CI_err": []}
+    
+    
     """
-    FOR LOOP to Perform several experiments varying hyperparameters
+    Name Specific Experiment based on argument parsed
     """
-    for sp_val in sp_variable_values:
-        config.K_support_points = sp_val
-        # for k_val in k_variable_values:
-        for neg_marg in neg_margin_values:
-            # config.K_knn = k_val
-            config.negative_margin = neg_marg
-            # config.specific_experiment_val = f"SP_{sp_val}_K_{k_val}"
-            config.specific_experiment_val = f"SP_{sp_val}_Neg_Marg_{str(neg_marg)[-1]}"
+    config.specific_experiment_val = args.experiment_name
+    logger.info(f"Working with Positive Margin: {config.positive_margin} || Negative Margin: {config.negative_margin}")
 
-            if k_val <= sp_val:
-                merged_variable_values.append(config.specific_experiment_val)
-                logger.info(f"Working with SUPPORT POINTS: {config.K_support_points} || K KNN: {config.K_knn}")
+    """
+    Load Data
+    """
+    train_dataset, val_dataset, test_dataset = myFinalDataset(config=config,
+                                                                logger=logger)._get_dataset()
 
-                """
-                Load Data
-                """
-                train_dataset, val_dataset, test_dataset = myFinalDataset(config=config,
-                                                                          logger=logger)._get_dataset()
+    """
+    Load Model
+    """
+    if "resnet" in config.model_name:
+        model, arch_name = ResNet(config=config,
+                                    logger=logger).load_pretrained_resnet_distance()
+        config.model_name = arch_name
+    else:
+        # Will implement functionality to work with different architectures
+        pass
+    
+    """
+    Train Model (or skip in case personal model is loaded)
+    """
+    if config.train_new_model:
+        trained_model, train_acc, val_acc, CI_err = myTrainerDistance(config=config,
+                                                                        logger=logger).train_model(model=model,
+                                                                                                    train_set=train_dataset,
+                                                                                                    val_set= val_dataset,
+                                                                                                    show_training_curves=True,
+                                                                                                    return_acc=True)
+        # Store Train and Validation Accuracy
+        # metrics_lists["train_acc"].append(train_acc)
+        metrics_lists["val_acc"].append(val_acc)
+        metrics_lists["CI_err"].append(CI_err)
+    else:
+        trained_model = model
+    
+    """
+    Evaluate Model
+    """
+    Evaluator(model=trained_model,
+                test_set=train_dataset,
+                config=config,
+                logger=logger).evaluate_distance_model()
 
-                """
-                Load Model
-                """
-                if "resnet" in config.model_name:
-                    model, arch_name = ResNet(config=config,
-                                              logger=logger).load_pretrained_resnet_distance()
-                    config.model_name = arch_name
-                else:
-                    # Will implement functionality to work with different architectures
-                    pass
-                
-                """
-                Train Model (or skip in case personal model is loaded)
-                """
-                if config.train_new_model:
-                    trained_model, train_acc, val_acc, CI_err = myTrainerDistance(config=config,
-                                                                                  logger=logger).train_model(model=model,
-                                                                                                             train_set=train_dataset,
-                                                                                                             val_set= val_dataset,
-                                                                                                             show_training_curves=True,
-                                                                                                             return_acc=True)
-                    # Store Train and Validation Accuracy
-                    # metrics_lists["train_acc"].append(train_acc)
-                    metrics_lists["val_acc"].append(val_acc)
-                    metrics_lists["CI_err"].append(CI_err)
-                else:
-                    trained_model = model
-                
-                """
-                Evaluate Model
-                """
-                Evaluator(model=trained_model,
-                          test_set=train_dataset,
-                          config=config,
-                          logger=logger).evaluate_distance_model()
-
-                """
-                Plot results with Confidence Interval
-                """
-                ResultsPlotter(config=config,
-                               logger=logger).plot_accuracies_w_CI(metrics_dict=metrics_lists,
-                                                                   variable_values=merged_variable_values)
+    """
+    Plot results with Confidence Interval
+    """
+    ResultsPlotter(config=config,
+                    logger=logger).plot_accuracies_w_CI(metrics_dict=metrics_lists,
+                                                        variable_values=[args.experiment_name])
 
 
 
--- prototype_learning.py
+++ prototype_learning.py
@@ -1,5 +1,6 @@
 
 import torch
+import argparse
 from src.model import *
 from scripts.evaluate import Evaluator
 from src.dataset import myProtoDataset
@@ -11,69 +12,79 @@ from src.results_plotter import ResultsPlotter
 
 
 if __name__ == "__main__":
+    """
+    Load Parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-exp_name", "--experiment_name", help="Experiment name for logging", type=str, required=True)
+    args = parser.parse_args()
+    print(args)
+
     torch.cuda.empty_cache()
+    """
+    Load Config file and Logger
+    """
     config = myConfig()
     logger = myLogger(logger_name=__name__, config=config).get_logger()
     torch.manual_seed(config.rand_seed)
-    # Load Data
-    
-    # variable_values = [128, 256, 512]
-    variable_values = ["resnet34", "resnet50"]
 
     metrics_lists  = {"train_acc": [],
                       "val_acc": [],
                       "CI_err": []}
+    
     """
-    FOR LOOP to Perform several experiments varying hyperparameters
+    Name Specific Experiment based on argument parsed
     """
-    for val in variable_values:
+    config.specific_experiment_val = args.experiment_name
+    logger.info(f"Working on: {args.experiment_name}")
         
-        # config.final_layer_dim = val
-        config.model_name = val
-        config.specific_experiment_val = f"Model_{val}"
+    """
+    Load Data
+    """
+    train_dataset, val_dataset, test_dataset = myProtoDataset(config=config,
+                                                              logger=logger)._get_dataset()
+    
+    """
+    Load Model
+    """
 
-        logger.info(f"Working with Model = {val}")
-        
-        """
-        Load Data
-        """
-        train_dataset, val_dataset, test_dataset = myProtoDataset(config=config, logger=logger)._get_dataset()
-      
-        """
-        Load Model
-        """
+    if "resnet" in config.model_name:
+        model, arch_name = ResNet(config=config,
+                                  logger=logger).load_pretrained_resnet_prototypes()
+        config.model_name = arch_name
+    else:
+        pass
+    
+    """
+    Train Model (or skip in case personal model is loaded)
+    """
+    if config.train_new_model: 
+        trained_model, train_acc, val_acc, CI_err = myTrainerProto(config=config,
+                                                                   logger=logger).train_model(model=model,
+                                                                                              train_set=train_dataset,
+                                                                                              val_set=val_dataset,
+                                                                                              show_training_curves=True,
+                                                                                              return_acc=True)
+        # Store Train and Validation Accuracy
+        metrics_lists["val_acc"].append(val_acc)
+        metrics_lists["CI_err"].append(CI_err)
+    else:
+        trained_model = model
+    
+    """
+    Evaluate Model
+    """
+    Evaluator(model=trained_model,
+              test_set=test_dataset,
+              config=config,
+              logger=logger).evaluate_proto_model()
 
-        if "resnet" in config.model_name:
-            model, arch_name = ResNet(config=config, logger=logger).load_pretrained_resnet_prototypes()
-            config.model_name = arch_name
-        else:
-            pass
-        
-        """
-        Train Model (or skip in case personal model is loaded)
-        """
-        if config.train_new_model: 
-            trained_model, train_acc, val_acc, CI_err = myTrainerProto(config=config, logger=logger).train_model(model=model,
-                                                                train_set=train_dataset,
-                                                                val_set=val_dataset,
-                                                                show_training_curves=True,
-                                                                return_acc=True)
-            # Store Train and Validation Accuracy
-            metrics_lists["val_acc"].append(val_acc)
-            metrics_lists["CI_err"].append(CI_err)
-        else:
-            trained_model = model
-        
-        """
-        Evaluate Model
-        """
-        Evaluator(model=trained_model,test_set=test_dataset, config=config, logger=logger).evaluate_proto_model()
-
-        """
-        Plot results with Confidence Interval
-        """
-        ResultsPlotter(config=config, logger=logger).plot_accuracies_w_CI(metrics_dict=metrics_lists,
-                                                                    variable_values=variable_values)
+    """
+    Plot results with Confidence Interval
+    """
+    ResultsPlotter(config=config,
+                   logger=logger).plot_accuracies_w_CI(metrics_dict=metrics_lists,
+                                                       variable_values=[args.experiment_name])
 
 
 
